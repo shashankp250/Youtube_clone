7740b126befee065d41fb0aef321dec50a130ce3
shoaibk <shashank.pawar@connvertex.com>
2017-10-12 18:19:28 +0530

    initial project setup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
+//React library is used for create and manage component
+//while ReactDOM library is used for manipulating DOM in order to render component
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+//create component, it should produce some html
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+ const App = () => {
+     return <div>Learning React</div>;
+ }
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+//take this component and put it in to the DOM
+
+ReactDOM.render(<App />, document.querySelector(".container"));
