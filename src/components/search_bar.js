04172f3b6afd48674fd07c431b239aaecaa92ad1
shashankp250 <shashank.pawar@connvertex.com>
2017-10-13 18:52:59 +0530

    modifying input value according to state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,16 +2,24 @@ import React, {Component} from 'react';
 
 class SearchBar extends Component {
 
+    //we initialize the state by defining the constructor
     constructor(props){
         super(props);
 
         this.state = {term :''};
-        //state is nothing but a plain js object
+
+        //state is nothing but a plain js object, each class based component has own copy of state
         // which is used to record and create values of property or variables of component
     }
 
     render(){
-     return <input onChange={event => console.log(event.target.value)}/>;
+     return (
+         <div>
+             <input
+                 value = {this.state.term}
+                 onChange={event => this.setState({term : event.target.value})}/>
+         </div>
+     );
     }
 }
 
