5df228fcceb56ef5792e55751b909e465a00d22b
Shashank <shashankpawar@Shashanks-MacBook-Air.local>
2017-10-14 20:48:35 +0530

    video list added, video details section added, css fixes

--- src/index.js
+++ src/index.js
@@ -1,18 +1,44 @@
 //React library is used for create and manage component
 //while ReactDOM library is used for manipulating DOM in order to render component
 
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+import YTSearch from 'youtube-api-search';
+const API_KEY = 'AIzaSyAvheYR8u1FUnDl_MLPKYKjSkQxEQp6OmI';
 //create component, it should produce some html
 
- const App = () => {
-     return (
+ class App extends Component {
+ 	constructor(props){
+ 		super(props);
+
+ 		this.state = { 
+ 			videos:[],
+ 			selectedVideo:null
+ 		};
+
+ 		YTSearch({key:API_KEY, term:'surfboards'}, (videos)=>{
+ 			this.setState({
+ 				videos:videos,
+ 				selectedVideo:videos[0]})});
+
+ 	}
+
+ 	render(){
+ 		return (
          <div>
              <SearchBar />
+             <VideoList 
+             	videos = {this.state.videos}
+             	onVideoSelect = {(selectedVideo) => this.setState({selectedVideo}) }/>
+             <VideoDetail video = {this.state.selectedVideo}/>
          </div>
      );
+ 	}
+
+     
  }
 
 //take this component and put it in to the DOM
